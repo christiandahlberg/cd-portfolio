8833f06a8c2dfb3a9d7ce25d90dc38678ffa2b8b
christiandahlberg <ch7040da-s@student.lu.se>
2020-07-22 21:59:25 +0200

    fix placeholder for upcoming projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -43,21 +43,12 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
+    title: 'TBD',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    repo: '', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
